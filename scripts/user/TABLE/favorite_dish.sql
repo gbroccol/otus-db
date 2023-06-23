2b5f66cabf656336b0d68f649ca01d4c7a32666c
Пчелина Анастасия <pchelina.a2@wildberries.work>
2023-06-24 01:12:44 +0200

    Userinfo

--- scripts/user/TABLE/favorite_dish.sql
+++ scripts/user/TABLE/favorite_dish.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS "user".favorite_dish
 (
     user_id      BIGINT      NOT NULL,
     dish_id      BIGINT      NOT NULL, -- dish_name + cooking_time + complexity (можно добавить в индекс для preview)
-    CONSTRAINT pk_1 PRIMARY KEY (user_id, dish_id)
+    CONSTRAINT pk_favorite_dish PRIMARY KEY (user_id, dish_id)
 );
