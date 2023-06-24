c384d2914e77191b7caa2d54d32948b47262fd35
Пчелина Анастасия <pchelina.a2@wildberries.work>
2023-06-24 10:21:06 +0200

    ingredient

--- scripts/ingredient/TABLE/ingredient.sql
+++ scripts/ingredient/TABLE/ingredient.sql
@@ -1,20 +1,10 @@
-CREATE TABLE ingredient.ingredient
+CREATE TABLE IF NOT EXISTS ingredient.ingredient
 (
-    ingredient_id bigint      NOT NULL,
-    name_rus      varchar(50) NOT NULL,
-    proteins      integer     NOT NULL,
-    fats          integer     NOT NULL,
-    carbohydrates integer     NOT NULL,
-    calories      integer     NOT NULL,
+    ingredient_id BIGINT      NOT NULL,
+    name          VARCHAR(50) NOT NULL, -- добавить в индекс, но не индексировать по полю (доп данные)
+    proteins      INTEGER     NOT NULL,
+    fats          INTEGER     NOT NULL,
+    carbohydrates INTEGER     NOT NULL,
+    calories      INTEGER     NOT NULL,
     CONSTRAINT pk_ingredient PRIMARY KEY (ingredient_id)
 );
-
-COMMENT ON TABLE ingredient.ingredient IS 'Ингредиенты для блюд
-Таблица включает в себы:
-- название продукта
-- кбжу';
-
-
-
-
-
