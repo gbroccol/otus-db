9a6bb5531779c10febf31ab58549118abbeef34c
Пчелина Анастасия <pchelina.a2@wildberries.work>
2023-06-24 10:18:49 +0200

    review

--- scripts/dish/TABLE/review.sql
+++ scripts/dish/TABLE/review.sql
@@ -1,26 +1,11 @@
-CREATE TABLE dish.review
+CREATE TABLE IF NOT EXISTS dish.review
 (
-    review_id bigint                   NOT NULL,
-    user_id   bigint                   NOT NULL,
-    recipe_id bigint                   NOT NULL,
-    message   text                     NOT NULL,
-    score     integer                  NOT NULL,
-    create_dt timestamp with time zone NOT NULL, -- заменить везде
-    edit_dt   timestamp with time zone      NULL,
+    review_id BIGSERIAL                NOT NULL,
+    user_id   BIGINT                   NOT NULL, -- кто создал отзыв
+    recipe_id BIGINT                   NOT NULL,
+    message   TEXT                     NOT NULL,
+    score     INTEGER                  NOT NULL, -- оценка (от 1 до 10)
+    create_dt TIMESTAMP WITH TIME ZONE NOT NULL, -- заменить везде
+    edit_dt   TIMESTAMP WITH TIME ZONE NULL,
     CONSTRAINT pk_review PRIMARY KEY (review_id)
 );
-
-COMMENT ON TABLE dish.review IS 'Отзывы на рецепты
-Таблица содержит след. информацию:
-- о том, кто создал отзыв
-- id рецепта
-- сообщение
-- оценка (от 1 до 10)
-- время создания отзыва
-- время редактирования';
-
-COMMENT ON COLUMN dish.review.score IS 'оценка от 1 до 10  (буду рисовать 5 звезд)';
-
-
-
-
