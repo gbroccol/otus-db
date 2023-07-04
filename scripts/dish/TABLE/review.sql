86921670a078a35ed81e976906b2a55d7a6e7b1e
Пчелина Анастасия <pchelina.a2@wildberries.work>
2023-07-04 20:55:23 +0200

    Edit dish schema

--- scripts/dish/TABLE/review.sql
+++ scripts/dish/TABLE/review.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS dish.review
     recipe_id BIGINT                   NOT NULL,
     message   TEXT                     NOT NULL,
     score     INTEGER                  NOT NULL, -- оценка (от 1 до 10)
-    create_dt TIMESTAMP WITH TIME ZONE NOT NULL, -- заменить везде
+    create_dt TIMESTAMP WITH TIME ZONE NOT NULL,
     edit_dt   TIMESTAMP WITH TIME ZONE NULL,
     CONSTRAINT pk_review PRIMARY KEY (review_id)
 );
