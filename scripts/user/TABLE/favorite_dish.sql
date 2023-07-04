9623144e494db4734432dbf3e37fd1e86be6e3a4
Пчелина Анастасия <pchelina.a2@wildberries.work>
2023-07-04 20:52:36 +0200

    Edit user schema

--- scripts/user/TABLE/favorite_dish.sql
+++ scripts/user/TABLE/favorite_dish.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "user".favorite_dish
 (
     user_id      BIGINT      NOT NULL,
-    dish_id      BIGINT      NOT NULL, -- dish_name + cooking_time + complexity (можно добавить в индекс для preview)
+    dish_id      BIGINT      NOT NULL, --TODO dish_name + cooking_time + complexity (можно добавить в индекс для preview)
     CONSTRAINT pk_favorite_dish PRIMARY KEY (user_id, dish_id)
 );
